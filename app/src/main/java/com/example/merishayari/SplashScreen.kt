77056640fee01a61751b8891b3d7a73da6f34a4e
peelu123 <raghuwanshipalash6@gmail.com>
2022-07-11 16:28:23 +0530

    Made Start Screen

--- app/src/main/java/com/example/merishayari/SplashScreen.kt
+++ app/src/main/java/com/example/merishayari/SplashScreen.kt
@@ -1,11 +1,18 @@
 package com.example.merishayari
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed(Runnable {
+            startActivity(Intent(this,StartActivity::class.java))
+            finish()
+        },3000)
     }
 }

374e722fea8ac017d993201937c19625c20a61e6
peelu123 <raghuwanshipalash6@gmail.com>
2022-07-12 14:23:05 +0530

    Added Navigation Drawer

--- app/src/main/java/com/example/merishayari/MainActivity.kt
+++ app/src/main/java/com/example/merishayari/MainActivity.kt
@@ -2,13 +2,14 @@ package com.example.merishayari
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.merishayari.databinding.ActivityStartBinding
+import com.example.merishayari.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityStartBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityStartBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }

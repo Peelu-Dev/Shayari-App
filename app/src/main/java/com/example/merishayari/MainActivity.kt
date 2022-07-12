32eb969a565e350f79f32555fd9d11c53c9c62e2
peelu123 <raghuwanshipalash6@gmail.com>
2022-07-12 14:45:36 +0530

    Completed drawer navigation

--- app/src/main/java/com/example/merishayari/MainActivity.kt
+++ app/src/main/java/com/example/merishayari/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.merishayari
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import com.example.merishayari.databinding.ActivityMainBinding
 
 
@@ -11,5 +16,65 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnMenu.setOnClickListener{
+            if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+                binding.drawerLayout.closeDrawer(GravityCompat.START)
+            }else{
+                binding.drawerLayout.openDrawer(GravityCompat.START)
+            }
+        }
+
+        binding.navigationLayout.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.Share -> {
+                    try {
+                        val shareIntent = Intent(Intent.ACTION_SEND)
+                        shareIntent.type = "text/plain"
+                        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "My application name")
+                        var shareMessage = "Install this App to get new hindi Shayariaan"
+                        shareMessage =
+                            """
+                            ${shareMessage}https://play.google.com/store/apps/details?id=${BuildConfig.APPLICATION_ID}
+                            
+                            
+                            """.trimIndent()
+                        shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage)
+                        startActivity(Intent.createChooser(shareIntent, "choose one"))
+                    } catch (e: Exception) {
+                        //e.toString();
+                    }
+                    true
+                }
+                R.id.rate -> {
+                    try {
+                        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$packageName")))
+                    } catch (e: ActivityNotFoundException) {
+                        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=$packageName")))
+                    }
+                    true
+                }
+                R.id.more -> {
+                    val uri = Uri.parse("market://details?id=com.wordel")
+                    val myAppLinkToMarket = Intent(Intent.ACTION_VIEW, uri)
+                    try {
+                        startActivity(myAppLinkToMarket)
+                    } catch (e: ActivityNotFoundException) {
+                        Toast.makeText(this, " unable to find market app", Toast.LENGTH_LONG).show()
+                    }
+                    true
+                }else -> {
+                    false
+                }
+            }
+        }
+
+    }
+    override fun onBackPressed() {
+        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        }else{
+            super.onBackPressed()
+        }
     }
 }

3a949f422ba3b0b8cf5443b66115fd05bace1f7f
peelu123 <raghuwanshipalash6@gmail.com>
2022-07-12 13:54:13 +0530

    Added header layout for drawer menu

--- app/src/main/java/com/example/merishayari/MainActivity.kt
+++ app/src/main/java/com/example/merishayari/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.merishayari
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.merishayari.databinding.ActivityStartBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStartBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }

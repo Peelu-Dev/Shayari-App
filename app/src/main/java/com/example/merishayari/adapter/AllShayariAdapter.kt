28525eb298ff1c5a3d44f92a51a9fedaa9be8cc9
peelu123 <raghuwanshipalash6@gmail.com>
2022-07-13 14:44:40 +0530

    Added Gradient Colors

--- app/src/main/java/com/example/merishayari/adapter/AllShayariAdapter.kt
+++ app/src/main/java/com/example/merishayari/adapter/AllShayariAdapter.kt
@@ -1,10 +1,12 @@
 package com.example.merishayari.adapter
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.merishayari.AllShayariActivity
+import com.example.merishayari.R
 import com.example.merishayari.databinding.ItemShayariBinding
 import com.example.merishayari.model.ShayariModel
 
@@ -21,9 +23,25 @@ class AllShayariAdapter(
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShayariViewHolder {
         // binding ShayariViewHolder with ItemShayariBinding
         return  ShayariViewHolder(ItemShayariBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+
+
     }
 
     override fun onBindViewHolder(holder: ShayariViewHolder, position: Int) {
+
+
+        if(position%5==0){
+            holder.binding.mainBackGRound.setBackgroundResource(R.drawable.gradient_1)
+        }else if(position%5==1){
+            holder.binding.mainBackGRound.setBackgroundResource(R.drawable.gradient_2)
+        }else if(position%5==2){
+            holder.binding.mainBackGRound.setBackgroundResource(R.drawable.gradient_3)
+        }else if(position%5==3){
+            holder.binding.mainBackGRound.setBackgroundResource(R.drawable.gradient_4)
+        }else if(position%5==4){
+            holder.binding.mainBackGRound.setBackgroundResource(R.drawable.gradient_5)
+        }
+
         holder.binding.itemShayari.text = shayariList[position].data.toString()
     }
 
